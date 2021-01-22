530cc4d2254a19fe7bc60870eb82f9187271cb3e
bernardobgam <40729003+bernardobgam@users.noreply.github.com>
2021-01-22 12:11:52 -0600

    Removed 3D

--- src/components/drivers/index.js
+++ src/components/drivers/index.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
 export { default as CsvViewer } from './csv-viewer';
-export { default as Photo360Viewer } from './photo360-viewer';
 export { default as PDFViewer } from './pdf-viewer';
 export { default as DocxViewer } from './docx-viewer';
 export { default as VideoViewer } from './video-viewer';
--- src/components/drivers/photo-viewer-wrapper.jsx
+++ src/components/drivers/photo-viewer-wrapper.jsx
@@ -2,17 +2,8 @@
 
 import React, { Component } from 'react';
 
-import * as THREE from 'three';
 import PhotoViewer from './photo-viewer';
-import Photo360Viewer from './photo360-viewer';
 import Loading from '../loading';
-
-function getPhotoDriver(width, height, fileType) {
-  if (fileType === 'jpg' && window.Math.abs((width / height) - 2) <= 0.01) {
-    return Photo360Viewer;
-  }
-  return PhotoViewer;
-}
 
 export default class PhotoViewerWrapper extends Component {
   constructor(props) {
@@ -27,39 +18,40 @@ export default class PhotoViewerWrapper extends Component {
 
   componentDidMount() {
     // spike on using promises and a different loader or adding three js loading manager
-    const loader = new THREE.TextureLoader();
-    loader.crossOrigin = '';
-    // load a resource
-    loader.load(
-      // resource URL
-      this.props.filePath,
-      // Function when resource is   loaded
-      (texture) => {
-        this.setState({
-          originalWidth: texture.image.width,
-          originalHeight: texture.image.height,
-          imageLoaded: true,
-          texture,
-        });
-      },
-      (xhr) => {
-        console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
-      },
-      (xhr) => {
-        console.log('An error happened', xhr);
-      },
-    );
+    // const loader = new THREE.TextureLoader();
+    // loader.crossOrigin = '';
+    // // load a resource
+    // loader.load(
+    //   // resource URL
+    //   this.props.filePath,
+    //   // Function when resource is   loaded
+    //   (texture) => {
+    //     this.setState({
+    //       originalWidth: texture.image.width,
+    //       originalHeight: texture.image.height,
+    //       imageLoaded: true,
+    //       texture,
+    //     });
+    //   },
+    //   (xhr) => {
+    //     console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
+    //   },
+    //   (xhr) => {
+    //     console.log('An error happened', xhr);
+    //   },
+    // );
+    this.setState({
+      imageLoaded: true,
+    });
   }
 
   render() {
     if (!this.state.imageLoaded) {
       return <Loading />;
     }
-    const { originalWidth, originalHeight } = this.state;
-    const PhotoDriver = getPhotoDriver(originalWidth, originalHeight, this.props.fileType);
 
     return (
-      <PhotoDriver {...this.state} {...this.props} />
+      <PhotoViewer {...this.state} {...this.props} />
     );
   }
 }
